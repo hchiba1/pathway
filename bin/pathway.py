afe9638bd4c9b1776e8a97adad305f87a5490054
Hirokazu Chiba <hchiba1@gmail.com>
2023-08-28 17:09:55 +0900

    Update bin/pathway.py to output CL

--- bin/pathway.py
+++ bin/pathway.py
@@ -8,20 +8,15 @@ parser.add_argument('-n', '--cores', type=int, default=1, help='')
 parser.add_argument('-v', '--verbose', action='store_true', help='')
 args = parser.parse_args()
 
-def read_patylist():
+descr2acc = {}
+descr2type = {}
+def read_pathlist():
     with open('pathlist.txt', 'r') as f:
         count = 0
         descr = ''
         acc = ''
-        dict = {}
         for line in f:
             line = line.rstrip()
-            if line.startswith('---'):
-                count += 1
-                if count == 1:
-                    continue
-            if count == 0:
-                continue
             m = re.match(r'^ID\s+(.*)$', line)
             if m:
                 descr = m.groups()[0]
@@ -34,24 +29,40 @@ def read_patylist():
             m = re.match(r'^AC\s+(.*)$', line)
             if m:
                 acc = m.groups()[0]
-                dict[descr] = acc
-    return dict
+                descr2acc[descr] = acc
+            m = re.match(r'^CL\s+(.*)\.$', line)
+            if m:
+                type = m.groups()[0]
+                descr2type[descr] = type
 
 
-def get_acc_from_dict(dict, description):
-    if dict.get(description) != None:
-        ret = dict.get(description)
+def get_type_from_dict(description):
+    if descr2type.get(description) != None:
+        ret = descr2type.get(description)
         m = re.match(r'^(.+): step \d+/\d+$', description)
         if m:
             core_descr = m.groups()[0]
-            ret = get_acc_from_dict(dict, core_descr) + ';' + ret
+            ret = get_type_from_dict(core_descr) + ';' + ret
+        return ret
+    else:
+        return 'NULL'
+    # return descr2type.get(description, 'NULL')
+
+
+def get_acc_from_dict(description):
+    if descr2acc.get(description) != None:
+        ret = descr2acc.get(description)
+        m = re.match(r'^(.+): step \d+/\d+$', description)
+        if m:
+            core_descr = m.groups()[0]
+            ret = get_acc_from_dict(core_descr) + ';' + ret
         return ret
     else:
         print(description, file=sys.stderr)
         return 'NULL'
 
 
-def get_acc_list_from_line(dict, line):
+def get_type_list_from_line(line):
     m = re.match(r'^(.+) \[regulation\]$', line)
     reg = ''
     if m:
@@ -60,7 +71,25 @@ def get_acc_list_from_line(dict, line):
     arr = line.split('; ')
     acc_list = []
     for i in arr:
-        acc = get_acc_from_dict(dict, i)
+        acc = get_type_from_dict(i)
+        if acc != '':
+            acc_list.append(acc)
+    ret = ";".join(acc_list)
+    if reg != '':
+        ret += ';' + reg
+    return ret
+
+
+def get_acc_list_from_line(line):
+    m = re.match(r'^(.+) \[regulation\]$', line)
+    reg = ''
+    if m:
+        line = m.groups()[0]
+        reg = 'R'
+    arr = line.split('; ')
+    acc_list = []
+    for i in arr:
+        acc = get_acc_from_dict(i)
         if acc != '':
             acc_list.append(acc)
     ret = ";".join(acc_list)
@@ -70,9 +99,10 @@ def get_acc_list_from_line(dict, line):
 
 
 def main():
-    dict = read_patylist()
+    read_pathlist()
     count = 0
     description = ''
+    type = ''
     for line in sys.stdin:
         line = line.rstrip()
         if len(line) == 0:
@@ -94,7 +124,7 @@ def main():
                 m = re.match(r'^(\S+)\s+\((\S+)\)$', i)
                 if m:
                     [mnemonic, uniprot_id] = m.groups()
-                    print(uniprot_id, mnemonic, acc_list, description, sep='\t')
+                    print(uniprot_id, mnemonic, acc_list, description, type, sep='\t')
                 else:
                     print('ERROR: ' + i, file=sys.stderr)
                     sys.exit(1)
@@ -103,7 +133,8 @@ def main():
             if ma:
                 description = ma.groups()[0]
                 description = description.lower()
-                acc_list = get_acc_list_from_line(dict, description)
+                acc_list = get_acc_list_from_line(description)
+                type = get_type_list_from_line(description)
             else:
                 print('ERROR: ' + line, file=sys.stderr)
                 sys.exit(1)
