a576d8191830d4178fb7423f50e0a0089f057f90
Hirokazu Chiba <hchiba1@gmail.com>
2023-11-08 14:10:28 +0900

    Update SPARQL in js/sparql.js

--- js/sparql.js
+++ js/sparql.js
@@ -1,14 +1,24 @@
 async function fetchDatabySPARQL(name) {
-  const endpointUrl = 'https://rdfportal.org/sib/sparql';
+  const endpointUrl = 'https://sparql.uniprot.org';
   const sparqlQuery = `
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX up: <http://purl.uniprot.org/core/>
 
-SELECT ?pathway
-FROM <http://sparql.uniprot.org/pathways>
+SELECT DISTINCT ?uniprot ?mnemonic ?protein_label ?taxid ?organism_name
 WHERE {
   ?pathway a up:Pathway ;
            rdfs:label "${name}" .
+  ?uniprot a up:Protein ;
+           up:mnemonic ?mnemonic ;
+           rdfs:label ?protein_label ;
+           up:organism ?taxid ;
+           up:annotation [
+             a up:Pathway_Annotation ;
+             rdfs:seeAlso ?pathway
+           ] .
+  ?taxid up:scientificName ?organism_name .
 }
+ORDER BY ?cientificName ?mnemonic
 `;
 
   const response = await fetch(endpointUrl, {
