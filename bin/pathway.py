12d887736ccdaa34b1313fed0951c07be75b4912
Hirokazu Chiba <hchiba1@gmail.com>
2024-12-03 15:47:04 +0900

    Refactor bin/pathway.py

--- bin/pathway.py
+++ bin/pathway.py
@@ -1,85 +1,70 @@
 #!/usr/bin/env python3
-import argparse
-import sys
 import re
+import sys
 
 descr2acc = {}
 descr2type = {}
-def read_pathlist():
-    with open('pathlist.txt', 'r') as f:
-        count = 0
-        descr = ''
-        acc = ''
-        for line in f:
-            line = line.rstrip()
-            m = re.match(r'^ID\s+(.*)$', line)
-            if m:
-                descr = m.groups()[0]
-                descr = descr.lower()
-                if descr.endswith('.'):
-                    descr = descr.rstrip('.')
-                else:
-                    print('ERROR: description does not end with period: ' + descr, file=sys.stderr)
-                    sys.exit(1)                    
-            m = re.match(r'^AC\s+(.*)$', line)
-            if m:
-                descr2acc[descr] = m.groups()[0]
-            m = re.match(r'^CL\s+(.*)\.$', line)
-            if m:
-                descr2type[descr] = m.groups()[0]
+
+with open('pathlist.txt', 'r') as f:
+    descr = ''
+    for line in f:
+        line = line.rstrip()
+        if m := re.match(r'^ID\s+(.*)$', line):
+            descr = m.group(1).lower()
+            if descr.endswith('.'):
+                descr = descr.rstrip('.')
+            else:
+                print('ERROR: description does not end with period: ' + descr, file=sys.stderr)
+                sys.exit(1)                    
+        if m := re.match(r'^AC\s+(.*)$', line):
+            descr2acc[descr] = m.group(1)
+        if m := re.match(r'^CL\s+(.*)\.$', line):
+            descr2type[descr] = m.group(1)
 
 
-def get_type_from_dict(description):
-    if descr2type.get(description) != None:
-        ret = descr2type.get(description)
-        m = re.match(r'^(.+): step \d+/\d+$', description)
-        if m:
-            core_descr = m.groups()[0]
-            ret = get_type_from_dict(core_descr) + ';' + ret
+def get_type(descr):
+    if descr2type.get(descr) != None:
+        ret = descr2type.get(descr)
+        if m := re.match(r'^(.+): step \d+/\d+$', descr):
+            ret = get_type(m.group(1)) + ';' + ret
         return ret
     else:
         return 'NULL'
 
 
-def get_acc_from_dict(description):
-    if descr2acc.get(description) != None:
-        ret = descr2acc.get(description)
-        m = re.match(r'^(.+): step \d+/\d+$', description)
-        if m:
-            core_descr = m.groups()[0]
-            ret = get_acc_from_dict(core_descr) + ';' + ret
-        return ret
+def get_acc(descr):
+    if descr2acc.get(descr) != None:
+        acc = descr2acc.get(descr)
+        if m := re.match(r'^(.+): step \d+/\d+$', descr):
+            acc = get_acc(m.group(1)) + ';' + acc
+        return acc
     else:
-        print(description, file=sys.stderr)
+        print(f'AC_NULL\t{descr}', file=sys.stderr)
         return 'NULL'
 
 
-def get_type_list_from_line(line):
-    line = line.lower()
-    m = re.match(r'^(.+) \[regulation\]$', line)
-    if m:
-        line = m.groups()[0]
-    arr = line.split('; ')
+def get_type_list(description):
+    description = description.lower()
+    if m := re.match(r'^(.+) \[regulation\]$', description):
+        description = m.group(1)
     list = []
-    for i in arr:
-        type = get_type_from_dict(i)
-        if type != '':
-            list.append(type)
+    for elem in description.split('; '):
+        pathway_type = get_type(elem)
+        if pathway_type != '':
+            list.append(pathway_type)
     ret = ";".join(list)
     return ret
 
 
-def get_acc_list_from_line(line):
-    line = line.lower()
-    m = re.match(r'^(.+) \[regulation\]$', line)
+def get_acc_list(description):
+    description = description.lower()
     reg = ''
-    if m:
-        line = m.groups()[0]
+    if m := re.match(r'^(.+) \[regulation\]$', description):
+        description = m.group(1)
         reg = 'R'
-    arr = line.split('; ')
     list = []
-    for i in arr:
-        acc = get_acc_from_dict(i)
+    for elem in description.split('; '):
+        acc = get_acc(elem)
         if acc != '':
             list.append(acc)
     ret = ";".join(list)
@@ -88,11 +73,10 @@ def get_acc_list_from_line(line):
     return ret
 
 
-read_pathlist()
 with open('pathway.txt', 'r') as f:
     count = 0
     description = ''
-    type = ''
+    pathway_type = ''
     for line in f:
         line = line.rstrip()
         if len(line) == 0:
@@ -106,24 +90,20 @@ with open('pathway.txt', 'r') as f:
         if count <= 2:
             continue
         if line.startswith(' '):
-            line = line.lstrip()
-            line = line.rstrip(',')
+            line = line.lstrip().rstrip(',')
             arr = line.split(', ')
-            for i in arr:
-                i = i.rstrip()
-                m = re.match(r'^(\S+)\s+\((\S+)\)$', i)
-                if m:
-                    [mnemonic, uniprot_id] = m.groups()
-                    print(uniprot_id, mnemonic, acc_list, description, type, sep='\t')
+            for item in arr:
+                item = item.rstrip()
+                if matched := re.match(r'^(\S+)\s+\((\S+)\)$', item):
+                    mnemonic, uniprot_id = matched.groups()
+                    print(uniprot_id, mnemonic, acc_list, description, pathway_type, sep='\t')
                 else:
-                    print('ERROR: ' + i, file=sys.stderr)
+                    print('ERROR: ' + item, file=sys.stderr)
                     sys.exit(1)
+        elif matched := re.match(r'^(\S.*)\.$', line):
+            description = matched.group(1)
+            acc_list = get_acc_list(description)
+            pathway_type = get_type_list(description)
         else:
-            ma = re.match(r'^(\S.*)\.$', line)
-            if ma:
-                description = ma.groups()[0]
-                acc_list = get_acc_list_from_line(description)
-                type = get_type_list_from_line(description)
-            else:
-                print('ERROR: ' + line, file=sys.stderr)
-                sys.exit(1)
+            print('ERROR: ' + line, file=sys.stderr)
+            sys.exit(1)
